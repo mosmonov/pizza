2d0465e57ebda8f40af0042c30edb5dc597792eb
mosmonov <mkstnani08@gmail.com>
2017-05-25 23:51:08 -0400

    Update Delivery.java

--- Delivery.java
+++ Delivery.java
@@ -6,7 +6,7 @@ import java.util.*;
  *
  * 
  */
-public class Courier {
+public class pizzaDelivery {
     public static void main(String[] args) {
         
         Pizza[] pizzas = {new Pizza("p1", Pizza.PIZZATYPE.SMALL), new Pizza("p2", Pizza.PIZZATYPE.MEDIUM), new Pizza("p3",Pizza.PIZZATYPE.LARGE), new Pizza("p4", Pizza.PIZZATYPE.SUPERLARGE), new Pizza("p5",Pizza.PIZZATYPE.MEDIUM)};
