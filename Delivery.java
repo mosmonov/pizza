ac8af15732b1164c64856a33f574502f01cf9825
mosmonov <mkstnani08@gmail.com>
2017-05-25 23:13:55 -0400

    Update Delivery.java

--- Delivery.java
+++ Delivery.java
@@ -9,7 +9,7 @@ import java.util.*;
 public class Courier {
     public static void main(String[] args) {
         
-        Pizza[] pizzas = {new Pizza("p1", Pizza.PIZZATYPE.SMALL), new Pizza("p2", Pizza.PIZZATYPE.MEDIUM), new Car("p3",Car.CARTYPE.LARGE), new Car("p4", Car.CARTYPE.SUPERLARGE), new Car("p5",Car.CARTYPE.MEDIUM)};
+        Pizza[] pizzas = {new Pizza("p1", Pizza.PIZZATYPE.SMALL), new Pizza("p2", Pizza.PIZZATYPE.MEDIUM), new Pizza("p3",Pizza.PIZZATYPE.LARGE), new Pizza("p4", Pizza.PIZZATYPE.SUPERLARGE), new Pizza("p5",Pizza.PIZZATYPE.MEDIUM)};
         Pizzeria dominoPizzeria = new Pizzeria();
         
         dominoPizzeria.setInventory(pizzas);
